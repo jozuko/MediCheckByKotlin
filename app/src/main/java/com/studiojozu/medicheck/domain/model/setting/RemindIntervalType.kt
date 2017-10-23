3aafe54df3489a342dd4cd3a496930a9357f9a31
jozuko <jozuko.dev@gmail.com>
2017-10-23 16:43:54 +0900

    Convert java to Kotlin

---
+++ app/src/main/java/com/studiojozu/medicheck/domain/model/setting/RemindIntervalType.kt
@@ -0,0 +1,80 @@
+package com.studiojozu.medicheck.domain.model.setting
+
+import android.content.ContentValues
+import android.content.Context
+import android.content.res.Resources
+import android.support.annotation.StringRes
+import com.studiojozu.common.domain.model.ADbType
+import com.studiojozu.medicheck.R
+import java.util.*
+
+/**
+ * 通知の繰り返し間隔を表す型クラス
+ */
+class RemindIntervalType @JvmOverloads constructor(mIntervalMinutes: Any = RemindIntervalType.RemindIntervalPattern.MINUTE_5) : ADbType<Int, RemindIntervalType>(), Comparable<RemindIntervalType> {
+    private val mValue: RemindIntervalPattern
+
+    override val dbValue: Int
+        get() = RemindIntervalPattern.valueOfIntervalMinutes(mValue)
+
+    override val displayValue: String
+        get() = throw RuntimeException("you need to call getDisplayValue(TakeIntervalModeType).")
+
+    init {
+        mValue = when (mIntervalMinutes) {
+            is RemindIntervalPattern -> mIntervalMinutes
+            is Long -> RemindIntervalPattern.typeOfIntervalMinute(mIntervalMinutes.toInt())
+            is Int -> RemindIntervalPattern.typeOfIntervalMinute(mIntervalMinutes)
+            else -> throw IllegalArgumentException("unknown type")
+        }
+    }
+
+    override fun setContentValue(columnName: String, contentValue: ContentValues) = contentValue.put(columnName, dbValue)
+
+    override fun compareTo(other: RemindIntervalType): Int = mValue.compareTo(other.mValue)
+
+    fun getDisplayValue(resources: Resources): String {
+        val stringRes = RemindIntervalPattern.valueOfStringRes(mValue)
+        return if (stringRes == 0) "" else resources.getString(stringRes, RemindIntervalPattern.valueOfDisplayValue(mValue))
+    }
+
+    enum class RemindIntervalPattern(internal val mIntervalMinutes: Int, private val mDisplayValue: Int, @param:StringRes @field:StringRes private val mStringRes: Int) {
+        MINUTE_1(1, 1, R.string.interval_minute),
+        MINUTE_5(5, 5, R.string.interval_minutes),
+        MINUTE_10(10, 10, R.string.interval_minutes),
+        MINUTE_15(15, 15, R.string.interval_minutes),
+        MINUTE_30(30, 30, R.string.interval_minutes),
+        HOUR_1(60, 1, R.string.interval_hour);
+
+        companion object {
+            internal fun valueOfIntervalMinutes(type: RemindIntervalPattern): Int = values().firstOrNull { it == type }?.mIntervalMinutes ?: 0
+            internal fun valueOfDisplayValue(type: RemindIntervalPattern): String = values().firstOrNull { it == type }?.mDisplayValue?.toString() ?: "0"
+            @StringRes
+            internal fun valueOfStringRes(type: RemindIntervalPattern): Int = values().firstOrNull { it == type }?.mStringRes ?: 0
+
+            internal fun typeOfIntervalMinute(intervalMinutes: Int): RemindIntervalPattern = values().firstOrNull { it.mIntervalMinutes == intervalMinutes } ?: RemindIntervalPattern.MINUTE_5
+        }
+    }
+
+    companion object {
+
+        private val serialVersionUID = -2200953636883165844L
+
+        /**
+         * 選択肢のIDとなる分とそれに対応する表示文字列をMapで返却する
+         *
+         * @param context アプリケーションコンテキスト
+         * @return 選択肢のIDとなる分とそれに対応する表示文字列のMap
+         */
+        fun getAllValues(context: Context): TreeMap<Int, String> {
+            val values = TreeMap<Int, String>()
+            for (remindIntervalPattern in RemindIntervalPattern.values()) {
+                val stringRes = RemindIntervalPattern.valueOfStringRes(remindIntervalPattern)
+                val stringResParam = RemindIntervalPattern.valueOfDisplayValue(remindIntervalPattern)
+                values.put(remindIntervalPattern.mIntervalMinutes, context.resources.getString(stringRes, stringResParam))
+            }
+
+            return values
+        }
+    }
+}
---
+++ app/src/test/java/com/studiojozu/medicheck/domain/model/setting/RemindIntervalTypeTest.kt
@@ -0,0 +1,221 @@
+package com.studiojozu.medicheck.domain.model.setting
+
+import android.content.ContentValues
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.powermock.core.classloader.annotations.PowerMockIgnore
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import java.util.*
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
+
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = "src/main/AndroidManifest.xml")
+@PowerMockIgnore("org.mockito.*", "org.robolectric.*", "android.*")
+class RemindIntervalTypeTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun constractor_NoParameter() {
+        val remindIntervalType = RemindIntervalType()
+        assertEquals(RemindIntervalType.RemindIntervalPattern.MINUTE_5, findProperty("mValue").call(remindIntervalType))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun constractor_RemindIntervalPattern() {
+        var param: RemindIntervalType.RemindIntervalPattern = RemindIntervalType.RemindIntervalPattern.MINUTE_1
+        assertEquals(param, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = RemindIntervalType.RemindIntervalPattern.MINUTE_5
+        assertEquals(param, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = RemindIntervalType.RemindIntervalPattern.MINUTE_10
+        assertEquals(param, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = RemindIntervalType.RemindIntervalPattern.MINUTE_15
+        assertEquals(param, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = RemindIntervalType.RemindIntervalPattern.MINUTE_30
+        assertEquals(param, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = RemindIntervalType.RemindIntervalPattern.HOUR_1
+        assertEquals(param, findProperty("mValue").call(RemindIntervalType(param)))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun constractor_Long() {
+        var param: Long = 1
+        assertEquals(RemindIntervalType.RemindIntervalPattern.MINUTE_1, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = 5
+        assertEquals(RemindIntervalType.RemindIntervalPattern.MINUTE_5, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = 10
+        assertEquals(RemindIntervalType.RemindIntervalPattern.MINUTE_10, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = 15
+        assertEquals(RemindIntervalType.RemindIntervalPattern.MINUTE_15, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = 30
+        assertEquals(RemindIntervalType.RemindIntervalPattern.MINUTE_30, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = 60
+        assertEquals(RemindIntervalType.RemindIntervalPattern.HOUR_1, findProperty("mValue").call(RemindIntervalType(param)))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun constractor_Int() {
+        @Suppress("RedundantExplicitType")
+        var param: Int = 1
+        assertEquals(RemindIntervalType.RemindIntervalPattern.MINUTE_1, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = 5
+        assertEquals(RemindIntervalType.RemindIntervalPattern.MINUTE_5, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = 10
+        assertEquals(RemindIntervalType.RemindIntervalPattern.MINUTE_10, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = 15
+        assertEquals(RemindIntervalType.RemindIntervalPattern.MINUTE_15, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = 30
+        assertEquals(RemindIntervalType.RemindIntervalPattern.MINUTE_30, findProperty("mValue").call(RemindIntervalType(param)))
+
+        param = 60
+        assertEquals(RemindIntervalType.RemindIntervalPattern.HOUR_1, findProperty("mValue").call(RemindIntervalType(param)))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun constractor_Unknown() {
+        try {
+            RemindIntervalType("1")
+            fail()
+        } catch (e: IllegalArgumentException) {
+            assertTrue(true)
+        }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun dbValue() {
+        assertEquals(1, RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_1).dbValue)
+        assertEquals(5, RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_5).dbValue)
+        assertEquals(10, RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_10).dbValue)
+        assertEquals(15, RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_15).dbValue)
+        assertEquals(30, RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_30).dbValue)
+        assertEquals(60, RemindIntervalType(RemindIntervalType.RemindIntervalPattern.HOUR_1).dbValue)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun displayValue() {
+        try {
+            RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_1).displayValue
+            fail()
+        } catch (e: RuntimeException) {
+            assertEquals("you need to call getDisplayValue(TakeIntervalModeType).", e.message)
+        }
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun setContentValue() {
+        val contentValue = ContentValues()
+        val columnName = "columnName"
+
+        contentValue.clear()
+        var remindIntervalType = RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_1)
+        remindIntervalType.setContentValue(columnName, contentValue)
+        assertEquals(1, contentValue.get(columnName))
+
+        contentValue.clear()
+        remindIntervalType = RemindIntervalType()
+        remindIntervalType.setContentValue(columnName, contentValue)
+        assertEquals(5, contentValue.get(columnName))
+
+        contentValue.clear()
+        remindIntervalType = RemindIntervalType(RemindIntervalType.RemindIntervalPattern.HOUR_1)
+        remindIntervalType.setContentValue(columnName, contentValue)
+        assertEquals(60, contentValue.get(columnName))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun compareTo() {
+        var remindIntervalType1 = RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_1)
+        var remindIntervalType2 = RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_1)
+        assertTrue(remindIntervalType1.compareTo(remindIntervalType2) == 0)
+
+        remindIntervalType1 = RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_1)
+        remindIntervalType2 = RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_5)
+        assertTrue(remindIntervalType1.compareTo(remindIntervalType2) < 0)
+
+        remindIntervalType1 = RemindIntervalType(RemindIntervalType.RemindIntervalPattern.HOUR_1)
+        remindIntervalType2 = RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_1)
+        assertTrue(remindIntervalType1.compareTo(remindIntervalType2) > 0)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    @Config(qualifiers = "ja")
+    fun getDisplayValue_ResourcesJa() {
+        val context = RuntimeEnvironment.application.applicationContext
+        var remindIntervalType = RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_1)
+        assertEquals("1分", remindIntervalType.getDisplayValue(context.resources))
+
+        remindIntervalType = RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_5)
+        assertEquals("5分", remindIntervalType.getDisplayValue(context.resources))
+
+        remindIntervalType = RemindIntervalType(RemindIntervalType.RemindIntervalPattern.HOUR_1)
+        assertEquals("1時間", remindIntervalType.getDisplayValue(context.resources))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun getDisplayValue_ResourcesDefaultLocale() {
+        val context = RuntimeEnvironment.application.applicationContext
+        var remindIntervalType = RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_1)
+        assertEquals("1 minute", remindIntervalType.getDisplayValue(context.resources))
+
+        remindIntervalType = RemindIntervalType(RemindIntervalType.RemindIntervalPattern.MINUTE_5)
+        assertEquals("5 minutes", remindIntervalType.getDisplayValue(context.resources))
+
+        remindIntervalType = RemindIntervalType(RemindIntervalType.RemindIntervalPattern.HOUR_1)
+        assertEquals("1 hour", remindIntervalType.getDisplayValue(context.resources))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    @Config(qualifiers = "ja")
+    fun getAllValues() {
+        val valueMap = RemindIntervalType.getAllValues(RuntimeEnvironment.application.applicationContext)
+
+        val expectMap = TreeMap<Int, String>()
+        expectMap.put(1, "1分")
+        expectMap.put(5, "5分")
+        expectMap.put(10, "10分")
+        expectMap.put(15, "15分")
+        expectMap.put(30, "30分")
+        expectMap.put(60, "1時間")
+
+        valueMap.entries.forEach { (k, v) ->
+            assertEquals(expectMap[k], v)
+        }
+    }
+
+    private fun findProperty(name: String): KProperty1<*, *> {
+        val mValueProperty = RemindIntervalType::class.memberProperties.find { it.name == name }
+        mValueProperty!!.isAccessible = true
+        return mValueProperty
+    }
+}
