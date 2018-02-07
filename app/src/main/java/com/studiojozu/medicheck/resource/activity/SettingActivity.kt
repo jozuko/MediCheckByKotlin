c3f676ae91264cd407cc5b8ca6a8853ebfa6d819
jozuko <jozuko.dev@gmail.com>
2018-02-08 07:51:29 +0900

    #8 Headerのメニューボタンを戻るボタンに変更

--- app/src/main/java/com/studiojozu/medicheck/resource/activity/SettingActivity.kt
+++ app/src/main/java/com/studiojozu/medicheck/resource/activity/SettingActivity.kt
@@ -7,12 +7,10 @@ import com.studiojozu.medicheck.R
 import com.studiojozu.medicheck.resource.custom.HeaderView
 import com.studiojozu.medicheck.resource.fragment.setting.SettingFragmentListener
 
-class SettingActivity : ABaseActivity(), SettingFragmentListener {
+class SettingActivity : ABaseActivity(), SettingFragmentListener, HeaderView.OnClickBackListener {
     companion object {
         const val TAG_CONTENTS = "tag_contents"
     }
-
-    private var currentFragmentType = SettingFragmentListener.SettingFragmentType.MENU
 
     @BindView(R.id.header_view)
     lateinit var headerView: HeaderView
@@ -24,23 +22,31 @@ class SettingActivity : ABaseActivity(), SettingFragmentListener {
         ButterKnife.bind(this)
 
         headerView.setParentActivity(this)
-        showChildFragment(currentFragmentType)
+        headerView.setOnClickBackListener(this)
+        showChildFragment(SettingFragmentListener.SettingFragmentType.MENU)
     }
 
-    override fun goNext(fragmentType: SettingFragmentListener.SettingFragmentType) {
-        currentFragmentType = fragmentType
-        showChildFragment(fragmentType)
-    }
+    override fun goNext(fragmentType: SettingFragmentListener.SettingFragmentType) =
+            showChildFragment(fragmentType)
 
     override fun goBack() =
             supportFragmentManager.popBackStack()
+
+    override fun onClickedHeaderBack() {
+        if (supportFragmentManager.backStackEntryCount == 0) {
+            setResult(ABaseActivity.RESULT_BACK_TO_MENU)
+            finish()
+            return
+        }
+        supportFragmentManager.popBackStack()
+    }
 
     private fun showChildFragment(nextFragmentType: SettingFragmentListener.SettingFragmentType) {
         val fragment = nextFragmentType.getFragment()
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.child_fragment, fragment, TAG_CONTENTS)
 
-        if (currentFragmentType != SettingFragmentListener.SettingFragmentType.MENU)
+        if (nextFragmentType != SettingFragmentListener.SettingFragmentType.MENU)
             fragmentTransaction.addToBackStack(null)
 
         fragmentTransaction.commit()
