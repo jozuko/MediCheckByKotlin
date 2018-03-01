de1c6078b65bcf47590a85c9013e33d3201a672a
jozuko <jozuko.dev@gmail.com>
2018-03-01 11:09:03 +0900

    #13 追加・編集のレイアウトを追加

--- app/src/main/java/com/studiojozu/medicheck/resource/fragment/setting/TimetableFragment.kt
+++ app/src/main/java/com/studiojozu/medicheck/resource/fragment/setting/TimetableFragment.kt
@@ -1,9 +1,15 @@
 package com.studiojozu.medicheck.resource.fragment.setting
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.support.annotation.StringRes
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.studiojozu.medicheck.R
@@ -25,6 +31,9 @@ class TimetableFragment : ABaseFragment() {
     @BindView(R.id.timetable_list_view)
     lateinit var timetableListView: SortableListView
 
+    @BindView(R.id.button_add)
+    lateinit var addButton: Button
+
     private var savedTimetables: MutableList<Timetable> = mutableListOf()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -35,6 +44,7 @@ class TimetableFragment : ABaseFragment() {
 
         updateSavedTimetable()
         setInitialData()
+        setAddButtonListener()
         return view
     }
 
@@ -50,36 +60,68 @@ class TimetableFragment : ABaseFragment() {
                     override fun getText(targetObject: Timetable): String = targetObject.timetableNameWithTime
                 },
                 onDeleteClickListener = object : SortableArrayAdapter.OnDeleteClickListener<Timetable> {
-                    override fun onDeleteClicked(targetObject: Timetable, position: Int): Boolean {
-                        val result = timetableRegisterService.delete(timetable = targetObject)
-                        if (result == TimetableRegisterService.ErrorType.NO_ERROR) return true
-
-                        if (result == TimetableRegisterService.ErrorType.LAST_ONE) {
-                            MessageAlertDialogFragment.build(this@TimetableFragment, {
-                                message = context.resources.getString(R.string.message_can_not_delete_last_one)
-                                positiveButtonLabel = context.resources.getString(android.R.string.ok)
-                            }).show(childFragmentManager, "delete_dialog")
-                            return false
-                        }
-
-                        MessageAlertDialogFragment.build(this@TimetableFragment, {
-                            message = context.resources.getString(R.string.message_can_not_delete_exist_relation_other)
-                            positiveButtonLabel = context.resources.getString(android.R.string.ok)
-                        }).show(childFragmentManager, "delete_dialog")
-                        return false
-                    }
+                    override fun onDeleteClicked(targetObject: Timetable, position: Int): Boolean = delete(targetObject)
                 })
 
-        timetableListView.setOnItemClickListener { parent, view, position, id ->
-            run {
-                TODO("not implemented")
-            }
-        }
+        timetableListView.setOnItemClickListener { _, _, position, _ -> showEditDialog(position) }
     }
 
     private fun updateSavedTimetable() {
         savedTimetables.clear()
         savedTimetables.addAll(timetableFinderService.findAll())
+    }
+
+    private fun showMessage(@StringRes resourceId: Int) =
+            MessageAlertDialogFragment.build(this@TimetableFragment, {
+                message = context.resources.getString(resourceId)
+                positiveButtonLabel = context.resources.getString(android.R.string.ok)
+            }).show(childFragmentManager, "message_dialog")
+
+    private fun setAddButtonListener() =
+            addButton.setOnClickListener { showEditDialog(-1) }
+
+    private fun delete(targetObject: Timetable): Boolean {
+        val result = timetableRegisterService.delete(timetable = targetObject)
+        if (result == TimetableRegisterService.ErrorType.NO_ERROR) return true
+
+        if (result == TimetableRegisterService.ErrorType.LAST_ONE) {
+            showMessage(R.string.message_can_not_delete_last_one)
+            return false
+        }
+
+        showMessage(R.string.message_can_not_delete_exist_relation_other)
+        return false
+    }
+
+    private fun showEditDialog(position: Int) {
+        val editFragment = EditFragment()
+        editFragment.positiveClickListener = DialogInterface.OnClickListener { _, _ -> save(position) }
+        editFragment.show(childFragmentManager, "edit_dialog")
+    }
+
+    private fun save(position: Int) {
+        // TODO 処理を書く
+    }
+
+    class EditFragment : DialogFragment(), DialogInterface.OnClickListener {
+
+        var positiveClickListener: DialogInterface.OnClickListener? = null
+
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            val builder = AlertDialog.Builder(activity, R.style.EditableAlertDialog)
+            builder.setView(R.layout.fragment_setting_alarm_edit)
+            builder.setTitle(R.string.button_setting_menu_take_time)
+            builder.setPositiveButton(android.R.string.ok, this@EditFragment)
+            builder.setNegativeButton(android.R.string.cancel, this@EditFragment)
+
+            return builder.create()
+        }
+
+        override fun onClick(dialog: DialogInterface?, which: Int) {
+            when (which) {
+                DialogInterface.BUTTON_POSITIVE -> positiveClickListener?.onClick(dialog, which)
+            }
+        }
     }
 }
 
